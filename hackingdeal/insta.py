38851c4844f3183adbb87249c4adbfb27b27a951
공채원 <chaewonkong@gmail.com>
2018-12-02 11:01:05 +0900

    Delete break

--- hackingdeal/insta.py
+++ hackingdeal/insta.py
@@ -38,17 +38,18 @@ try:
     for person in followings:
         if person not in followers:
             print(person)
-            break
-        else:
-            print("Okay")
 
 except:
     print("Failed!")
 else:
-    print("Success!")
+    print("Success! Printed All lists")
 
     #<a class="FPmhX notranslate _0imsa " title="leonkkkkk" href="/leonkkkkk/">leonkkkkk</a>
     #<a class="FPmhX notranslate _0imsa " title="ah___young_b" href="/ah___young_b/">ah___young_b</a>
     # /html/body/div[3]/div/div/div[2]/ul/div/li[1]/div/div[2]/div[1]/div/div/a
     # /html/body/div[3]/div/div/div[2]/ul/div/li/div/div/div/div/div/a
     # /html/body/div[3]/div/div/div[2]/ul/div/li/div/div[2]/div[1]/div/div/a
+
+# 인스타 팔로워 수/팔로잉 수 많을 경우 제대로 프린트 안 되는 문제!
+# 스크롤 해야만 더 볼 수 있기 때문으로 확인.
+# 셀레니움으로 스크롤하면서 긁어올 수 있는 방법 확인해야 함.
